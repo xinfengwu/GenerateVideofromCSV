f348a81e88912eb791ec9a161b21f4f8825b3347
xinfeng <xinfengcanada@gmail.com>
2024-07-07 13:32:45 +0800

    version 1.1

--- main.py
+++ main.py
@@ -6,68 +6,81 @@ from myutil import *
 # 主函数
 def main():
     # 构建项目文件夹路径
-    folder_path = os.path.join(os.getcwd(), "nihon", "Lesson1") 
-    root_folder = create_folder(folder_path)
-# 读取 CSV 文件
-    data = read_csv_data(os.path.join(root_folder, "data.csv"))
+    root_folder_name = "30天背完小学英语单词"
+    level2_folder_name = "Lesson1"
     
-# csv to mp3
-    # 创建文件夹body_mp3文件夹
-    body_mp3_folder = create_folder(os.path.join(root_folder, "body_mp3")) 
-    for row in data:
-        keyword = row['kanji'].strip()
-        file_name = row['index'].strip() + ".mp3"
-        output_file = os.path.join(body_mp3_folder, file_name)
-        text_to_mp3(keyword,output_file)
+    root_folder_path = os.path.join(os.getcwd(), root_folder_name) 
+    level2_folder_path = os.path.join(os.getcwd(), root_folder_name, level2_folder_name) 
+    
+    root_folder = create_folder(root_folder_path)
+    leve2_folder = create_folder(level2_folder_path)
+    
+    output_mp4 = os.path.join(root_folder, "Lesson1.mp4")
+# 读取 CSV 文件
+    data = read_csv_data(os.path.join(root_folder, "day1.csv"))
     
     
 # csv to ppt
     print("csv to ppt")
     # 生成正文ppt
     body_ppt_template = os.path.join(root_folder, "body_template.pptx")
-    body_output_ppt= os.path.join(root_folder, "body.pptx")
-    create_ppt_with_csv(data, body_ppt_template, body_output_ppt)
+    body_output_ppt = os.path.join(root_folder, "body.pptx")
+    # 新幻灯片背景图片
+    bg_img_path = ""
+    new_Presentation = create_ppt_with_csv(data, body_ppt_template, bg_img_path, body_output_ppt)
     
-    # 生成封面封底ppt
+    new_slide_width = new_Presentation.slide_width
+    new_slide_height = new_Presentation.slide_height
+
+
+    # 封面封底ppt模板
     covers_ppt_template = os.path.join(root_folder, "covers_template.pptx")
-    # covers_output_ppt = "covers.pptx"
-    covers_output_ppt = os.path.join(root_folder, "covers_template.pptx")
-    # print(covers_output_ppt)
-   
-
-# Create a 3-second silent MP3 file
-    silence_mp3 = create_silent_mp3(3, os.path.join(body_mp3_folder,"0.mp3"))
-    # print(silence_mp3)
+    
 
     
-# ppt to img
+# pppt---> pdf ---> img
+    print("ppt to img")
     # 创建文件夹pdf
-    pdf_folder = create_folder(os.path.join(root_folder, "pdf"))
+    pdf_folder = create_folder(os.path.join(leve2_folder, "pdf"))
     body_pdf = os.path.join(pdf_folder, "body.pdf")
     # 转换正文ppt---> pdf ---> img
-    body_pdf_file = ppt_to_pdf_by_soffice(body_output_ppt, pdf_folder, body_pdf)
+    # ppt_to_pdf_by_soffice(body_output_ppt, pdf_folder)
+    ppt_to_pdf_by_unoconv(body_output_ppt, body_pdf)
     # 创建文件夹body_img
-    body_img_folder = create_folder(os.path.join(root_folder, "body_img"))
-    pdf_to_img(body_pdf_file, body_img_folder)
+    body_img_folder = create_folder(os.path.join(leve2_folder, "body_img"))
+    pdf_to_img(body_pdf, body_img_folder)
     
     # 创建文件夹covers_img
-    covers_img_folder = create_folder(os.path.join(root_folder, "covers_img"))
+    covers_img_folder = create_folder(os.path.join(leve2_folder, "covers_img"))
     # 转换封面封底ppt---> pdf ---> img
     covers_pdf = os.path.join(pdf_folder, "covers_template.pdf")
-    covers_pdf_file = ppt_to_pdf_by_soffice(covers_output_ppt, pdf_folder, covers_pdf)
-    pdf_to_img(covers_pdf_file,covers_img_folder)
+    ppt_to_pdf_by_soffice(covers_ppt_template, pdf_folder, covers_pdf)
+    pdf_to_img(covers_pdf,covers_img_folder)
     
+    
+# csv to mp3
+    print("csv to mp3")
+    # 创建文件夹body_mp3文件夹
+    body_mp3_folder = create_folder(os.path.join(leve2_folder, "body_mp3")) 
+    for row in data:
+        keyword = row['kanji'].strip()
+        file_name = row['index'].strip() + ".mp3"
+        output_file = os.path.join(body_mp3_folder, file_name)
+        text_to_mp3(keyword, 'en', output_file)
+        
     
 # img + mp3 to video
+    print("img + mp3 to video")
     # 转换body img---> video
     # 创建文件夹body_mp4
-    body_mp4_folder = create_folder(os.path.join(root_folder, "body_mp4"))
-    print("Starting mp4")
+    body_mp4_folder = create_folder(os.path.join(leve2_folder, "body_mp4"))
+    # print("Starting mp4")
     # 执行比较
     common_files, unique_to_mp3, unique_to_img = compare_mp3_and_img_files(body_mp3_folder, body_img_folder)
-    print(common_files)
-    # 视频解析度要为2的倍数
-    resolution=[1240,2204]
+    # print(common_files)
+    
+    resolution = set_video_resolution(new_slide_width, new_slide_height)
+    
     for name in common_files:
         mp3_file = os.path.join(body_mp3_folder, name + '.mp3')
         img_file = os.path.join(body_img_folder, name + '.jpg')
@@ -76,28 +89,28 @@ def main():
     
     # 转换封面封底img---> video
     # 创建文件夹covers_mp4
-    covers_mp4_folder = create_folder(os.path.join(root_folder, "covers_mp4"))
+    covers_mp4_folder = create_folder(os.path.join(leve2_folder, "covers_mp4"))
     
     cover_img = os.path.join(covers_img_folder, "1.jpg")
     cover_mp4 = os.path.join(covers_mp4_folder, "1.mp4")
-    image_to_video(cover_img, resolution, cover_mp4)
+    image_to_video(cover_img, resolution, 3, cover_mp4)
     
     back_cover_img = os.path.join(covers_img_folder, "2.jpg")
     back_cover_mp4 = os.path.join(covers_mp4_folder, "2.mp4")
-    image_to_video(back_cover_img, resolution, back_cover_mp4)
+    image_to_video(back_cover_img, resolution, 3, back_cover_mp4)
         
         
 # video1 + video2 + ... to videos
-    output_mp4 = os.path.join(root_folder, "output.mp4")
-    
+    print("video1 + video2 + ... to videos")
     body_mp4_files = create_mp4_filelist(body_mp4_folder)
     # 将列表里的每个视频文件重复3次
-    repeated_mp4_files = repeat_elements(body_mp4_files, 1)
+    repeated_mp4_files = repeat_elements(body_mp4_files, 3)
     # print(repeated_mp4_files)
 
-    # 在每个列表的视频文件后插入静音视频间隔
-    silence_file = os.path.join(root_folder, "silence.mp4") 
-    create_silent_video(3, resolution, silence_file)
+    # 在每个列表的视频文件后插入2秒静音视频间隔
+    silence_file = os.path.join(root_folder, "silence.mp4")
+    gap_image_path = os.path.join(level1_folder_name, "gap.jpg")
+    image_to_video(gap_image_path, resolution, 2, silence_file)
     result_files = add_elements_with_silence(repeated_mp4_files, silence_file)
     
     # 将封面、封底分别插入列表首尾位置
@@ -112,10 +125,23 @@ def main():
         for item in result_files:
             file.write(f"file {item}\n")
     # print(result_files)
-    print("文件写入完成")
+    # print("文件写入完成")
 
     # 合成 封面+正文+封底 视频
     concatenate_mp4_files(input_txt, output_mp4)
+    
+    # 删除临时文件及文件夹
+    # delete_folder(body_mp3_folder)
+    delete_folder(body_mp4_folder)
+    # delete_folder(body_img_folder)
+    delete_folder(covers_img_folder)
+    delete_folder(covers_mp4_folder)
+    delete_folder(pdf_folder)
+    
+    delete_file(body_output_ppt)
+    delete_file(input_txt)
+    delete_file(silence_file)
+    
 
 # if __name__ == "__main__":
 main()
--- myutil.py
+++ myutil.py
@@ -15,17 +15,10 @@ from pdf2image.exceptions import (
 import subprocess
 from PIL import Image
 from pptx.util import Pt
-
-
-# 读取csv文件
-
-def read_csv_data(file_path):
-    data = []
-    with open(file_path, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            data.append(row)
-    return data
+from pptx.util import Inches
+from pptx.enum.shapes import MSO_SHAPE_TYPE
+from pptx.dml.color import RGBColor
+import shutil
 
 
 # 创建文件夹
@@ -55,57 +48,24 @@ def empty_folder(folder_path):
         elif os.path.isdir(file_path):
             empty_folder(file_path)
             
-
-# 按行生成语音
-def text_to_mp3(keyword, output_file):
-    if not os.path.exists(output_file):
-        try:
-        # print(gtts.lang.tts_langs()) 输出支持的语言
-            tts = gtts.gTTS(keyword, lang='ja')  ##  request google to get synthesis
-            tts.save(output_file)  ##  save audio
-        except Exception as e:
             
-            # line = row['kanji']
-            print(f"转换 '{row['kanji'].strip()}' 出现错误：{e}")
-            
-        
-def create_silent_mp3(duration, output_file):
-    # Construct the ffmpeg command
-    command = [
-        'ffmpeg', 
-        '-loglevel', 'error',
-        '-f', 'lavfi', 
-        '-i', 'anullsrc=r=44100:cl=stereo', 
-        '-t', str(duration), 
-        '-y', # 覆盖原有文件
-        output_file
-    ]
-
-    # Run the command
-    subprocess.run(command, check=True)
-    print("Done! Create silent mp3")
-    return output_file
-    
-    
-# 定义一个函数，将数据填入幻灯片
-def fill_slide(slide, row):
-    slide_title = find_shape_by_name(slide.shapes,'index')
-    add_text(slide_title,row['index'])
-    slide_title = find_shape_by_name(slide.shapes,'hiragana')
-    add_text(slide_title,row['hiragana'])
-    slide_title = find_shape_by_name(slide.shapes,'kanji')
-    add_text(slide_title,row['kanji'])
+# 读取csv文件
+def read_csv_data(file_path):
+    data = []
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data     
 
 
-# csv to ppt
-def create_ppt_with_csv(data, ppt_template, output_ppt):
+def create_ppt_with_csv(data, ppt_template, bg_img_path, output_ppt):
     prs = Presentation(ppt_template)
-    new_prs = Presentation()
 
     # 遍历 CSV 文件中的每条记录，创建对应的幻灯片
     for row in data:
         # 从模板创建一张新的幻灯片
-        new_slide = duplicate_slide(prs, 0)
+        new_slide = duplicate_slide(prs, 0, bg_img_path)
         # 填充文本
         fill_slide(new_slide, row)
 
@@ -113,32 +73,59 @@ def create_ppt_with_csv(data, ppt_template, output_ppt):
     delete_slide(prs,0)
     # 保存修改后的幻灯片
     prs.save(output_ppt)
-    print(f'幻灯片已保存为 {output_ppt}')
+    # print(f'幻灯片已保存为 {output_ppt}')
+    return prs
 
-
+   
 # 复制幻灯片
-def duplicate_slide(prs, index):
+def duplicate_slide(prs, index, bg_img_path):
     slide_to_copy = prs.slides[index]
+
     # Create a new slide object
     new_slide = prs.slides.add_slide(slide_to_copy.slide_layout)
-    # Copy content from the original slide to the new slide
+    
+    # 设置背景图片
+    if bg_img_path:
+        set_background_image(prs, new_slide, bg_img_path)
+    
+    # create images dict
+    imgDict = {}
+    
+    # 复制各种形状
     for shape in slide_to_copy.shapes:
-        el = shape.element
-        newel = copy.deepcopy(el)
-        new_slide.shapes._spTree.insert_element_before(newel, 'p:extLst')
+        # 使用 shape_type 属性判断形状类型
+        if shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
+            # print("This is a picture.")
+            # save image
+            with open(shape.name+'.jpg', 'wb') as f:
+                f.write(shape.image.blob)
+            # add image to dict
+            imgDict[shape.name+'.jpg'] = [shape.left, shape.top, shape.width, shape.height]
+        elif shape.shape_type == 1: # MSO_SHAPE_TYPE.AUTO_SHAPE:
+            # print("This is a AUTO_SHAPE.")
+            el = shape.element
+            newel = copy.deepcopy(el)
+            new_slide.shapes._spTree.insert_element_before(newel, 'p:extLst')
+        else:
+            print(f"This is an unhandled shape type: {shape.shape_type}")
+            
+    
+    # add pictures
+    for k, v in imgDict.items():
+        new_slide.shapes.add_picture(k, v[0], v[1], v[2], v[3])
+        os.remove(k)
 
     return new_slide
     
-
-# 删除幻灯片
-def delete_slide(prs, slide_index):
-    xml_slides = prs.slides._sldIdLst  # Access the slide list XML elements
-    if slide_index < 0 or slide_index >= len(xml_slides):
-        print(f"Slide index {slide_index} out of range.")
-        return
-    slide_id = xml_slides[slide_index].rId
-    prs.slides._sldIdLst.remove(xml_slides[slide_index])  # Remove slide from XML elements
-    prs.part.drop_rel(slide_id)  # Remove slide from related parts
+    
+# 将数据填入幻灯片
+def fill_slide(slide, row):
+    slide_title = find_shape_by_name(slide.shapes,'index')
+    add_text(slide_title,row['index'])
+    slide_title = find_shape_by_name(slide.shapes,'hiragana')
+    add_text(slide_title,row['hiragana'])
+    slide_title = find_shape_by_name(slide.shapes,'kanji')
+    add_text(slide_title,row['kanji'])
 
 
 # 查找文本框
@@ -152,68 +139,55 @@ def find_shape_by_name(shapes, name):
 # 修改文字
 def add_text(shape, text,):
     tf = shape.text_frame
-    tf.clear()
-    run = tf.paragraphs[0].add_run()
-    run.text = text if text else ''
-    font=run.font
-    font.name = 'Calibri'
-    font.size = Pt(58)
-    font.bold = True
+    p = tf.paragraphs[0].runs
+    p[0].text = text if text else ''
+
+   
+# 设置幻灯片背景
+def set_background_image(prs, new_slide, bg_img_path):
+    left = top = Inches(0)
+    pic = new_slide.shapes.add_picture(bg_img_path, left, top, width=prs.slide_width, height=prs.slide_height)
+
+    # This moves it to the background
+    new_slide.shapes._spTree.remove(pic._element)
+    new_slide.shapes._spTree.insert(2, pic._element)
+   
+   
+# 删除幻灯片
+def delete_slide(prs, slide_index):
+    xml_slides = prs.slides._sldIdLst  # Access the slide list XML elements
+    if slide_index < 0 or slide_index >= len(xml_slides):
+        print(f"Slide index {slide_index} out of range.")
+        return
+    slide_id = xml_slides[slide_index].rId
+    prs.slides._sldIdLst.remove(xml_slides[slide_index])  # Remove slide from XML elements
+    prs.part.drop_rel(slide_id)  # Remove slide from related parts
+    
     
 # 将ppt转化成pdf
+# pdf页面和对应幻灯片相比会有一点变形
 def ppt_to_pdf_by_soffice(ppt_file, pdf_folder, pdf_file):
     # kill all the soffice
-    # kill_cmd = "ps aux | grep -i office | awk {'print $2'} | xargs kill -9"
     kill_soffice_cmd = "killall soffice.bin"
-    # subprocess.call(kill_cmd, shell=True)
     soffice_cmd = "soffice --headless --convert-to pdf " + ppt_file + " --outdir " + pdf_folder
-    print("ppt to pdf")
     subprocess.call(soffice_cmd, shell=True)
-    # subprocess.call(kill_cmd, shell=True)
-    # name, ext = os.path.splitext(ppt_file)
-    # pdf_file = name + ".pdf"
-    print(f'幻灯片已导出为PDF ')
+
     return pdf_file
     
-    
-# ppt to pdf by pptxtopdf
-# pip install comtypes only for windows
-# pip install pptxtopdf
-def ppt_to_pdf_for_windows():
-    convert(ppt_file, output_dir)
-    
-    
-# ppt to pdf by unoconv on linux
-# sudo apt-get install unoconv
-def ppt_to_pdf_by_unoconv(ppt_file):
-    unoconv_cmd = [
-        "unoconv -f pdf",
-        "-o",
-        "./",
-        ppt_file
-    ]
+
+def ppt_to_pdf_by_unoconv(ppt_file, body_pdf):
+    # subprocess.call(['unoconv', '-f', 'pdf', '-o', pdf_folder, ppt_file])
+    unoconv_cmd = "unoconv -f pdf -o " + body_pdf + " " + ppt_file
     subprocess.call(unoconv_cmd, shell=True)
-    name, ext = os.path.splitext(ppt_file)
-    pdf_file = name + ".pdf"
-    print(f'幻灯片已导出为PDF ')
-    return pdf_file
-    
-    
+
 # 将pdf转化成jpg
-def pdf_to_img(pdf_file,imgs_folder):
+def pdf_to_img(pdf_file, imgs_folder):
     pages = convert_from_path(pdf_file, first_page=0)
-    print("Pages done")
     for img in pages:
         img_path = imgs_folder + '/' + str(pages.index(img)+1) + ".jpg"
         img.save(img_path , quality=100)
-    print(f'PDF已导出为图片 ')
+    # print(f'PDF已导出为图片 ')
 
-
-def filter_files_by_extension(folder, extension):
-    # 获取指定文件夹中特定后缀名的文件列表
-    file_list = [file for file in os.listdir(folder) if file.endswith(extension)]
-    return file_list
-    
 
 def compare_mp3_and_img_files(mp3_folder, img_folder):
     # 筛选出 mp3_folder 中所有的 mp3 文件和 img_folder 中所有的 img 文件
@@ -239,6 +213,94 @@ def compare_mp3_and_img_files(mp3_folder, img_folder):
 
     return common_files, unique_to_mp3, unique_to_img
     
+
+def filter_files_by_extension(folder, extension):
+    # 获取指定文件夹中特定后缀名的文件列表
+    file_list = [file for file in os.listdir(folder) if file.endswith(extension)]
+    return file_list
+    
+
+# 按行生成语音
+def text_to_mp3(keyword, language, output_file):
+    if not os.path.exists(output_file):
+        try:
+        # print(gtts.lang.tts_langs()) 输出支持的语言
+            tts = gtts.gTTS(keyword, lang=language)  ##  request google to get synthesis
+            tts.save(output_file)  #  save audio
+        except Exception as e:
+            print(f"转换 '{row['kanji'].strip()}' 出现错误：{e}")
+
+
+def set_video_resolution(slide_width, slide_height):
+    # 常量
+    EMU_PER_INCH = 914400
+    PIXELS_PER_INCH = 96
+
+    # 获取幻灯片宽度和高度（EMU）
+    slide_width_emu = slide_width
+    slide_height_emu = slide_height
+
+    # 将 EMU 转换为英寸
+    slide_width_inches = slide_width_emu / EMU_PER_INCH
+    slide_height_inches = slide_height_emu / EMU_PER_INCH
+
+    # 将英寸转换为像素
+    slide_width_pixels = int(slide_width_inches * PIXELS_PER_INCH)
+    slide_height_pixels = int(slide_height_inches * PIXELS_PER_INCH)
+
+    # 视频解析度要为2的倍数
+    if slide_width_pixels % 2 != 0:
+        slide_width_pixels += 1
+    if slide_height_pixels % 2 != 0:
+        slide_height_pixels += 1
+        
+    print(f"Slide width: {slide_width_emu} EMU, {slide_width_pixels:.2f} pixels")
+    print(f"Slide height: {slide_height_emu} EMU, {slide_height_pixels:.2f} pixels")
+    
+    resolution = [slide_width_pixels,slide_height_pixels]
+    return resolution   
+    
+    
+# img + mp3 ---> video
+def img_mp3_to_mp4(mp3_file, img_file, size, mp4_file):
+    # print("img_mp3_to_mp4")
+    ffmpeg_cmd = (
+        f"ffmpeg -loglevel error  -i {mp3_file} "
+        f" -loop 1 -i {img_file}"
+        f" -vcodec libx264 -pix_fmt yuv420p -shortest -y "
+        f" -vf 'scale={size[0]}:{size[1]}' {mp4_file}"
+    )
+    # print(ffmpeg_cmd)
+    subprocess.call(ffmpeg_cmd, shell=True)
+    
+    
+# img ---> video
+def image_to_video(image_path, size, duration, output_file):
+    # FFmpeg 命令
+    ffmpeg_command = (
+        f"ffmpeg -loglevel error -loop 1 -t {duration} -i {image_path}"
+        f" -f lavfi -i anullsrc=cl=mono:r=24000 -shortest -y "
+        f" -vf 'scale={size[0]}:{size[1]}' {output_file}"
+        
+    )
+    
+    # 运行 FFmpeg 命令
+    subprocess.run(ffmpeg_command, shell=True, check=True)   
+   
+    
+# 创建mp4_filelist.txt
+def create_mp4_filelist(folder_path):
+    # 获取文件夹中所有的 MP4 文件,
+    mp4_filenames_set = get_mp4_filenames(folder_path)
+    
+    # 排序
+    sorted_mp4_filenames = sort_filenames_as_integers(mp4_filenames_set)
+    
+    # 组成文件完整路径
+    mp4_files = add_path_to_filenames(folder_path, sorted_mp4_filenames)
+    
+    return mp4_files
+
 
 # 获取文件夹下所有文件的文件名和扩展名，并保存到set中
 def get_mp4_filenames(folder_path):
@@ -267,27 +329,13 @@ def sort_filenames_as_integers(filenames_set):
     
     return sorted_filenames
     
-
+    
 def add_path_to_filenames(folder_path, filenames):
     full_paths = []
     for filename in filenames:
         full_path = os.path.join(folder_path, filename + '.mp4')
         full_paths.append(full_path)
-    return full_paths
-    
-
-# 创建mp4_filelist.txt
-def create_mp4_filelist(folder_path):
-    # 获取文件夹中所有的 MP4 文件,
-    mp4_filenames_set = get_mp4_filenames(folder_path)
-    
-    # 排序
-    sorted_mp4_filenames = sort_filenames_as_integers(mp4_filenames_set)
-    
-    # 组成文件完整路径
-    mp4_files = add_path_to_filenames(folder_path, sorted_mp4_filenames)
-    
-    return mp4_files
+    return full_paths    
     
     
 # 将列表中的每个元素重复 n 次
@@ -305,54 +353,7 @@ def add_elements_with_silence(input_list, silence_file):
     
     return result
     
-
-# 创建空白静音视频
-def create_silent_video(duration, size, output_file):
-    # 创建一个白色的静音视频
-    # :s=2204x1240
-    ffmpeg_command = (
-        f"ffmpeg -loglevel error -f lavfi -i color=c=white:s={size[0]}x{size[1]}:d={duration} " #2204x1240
-        f"-f lavfi -t 3 -i anullsrc=r=24000:cl=mono "
-        f" -c:v libx264 -c:a aac -y {output_file}"
-    )
-    # -i input.mp4：指定输入视频文件。
-    # -f lavfi -t 3 -i anullsrc=r=44100:cl=stereo：
-    # -f lavfi：指定输入格式为Libavfilter。
-    # -t 3：将输入流的持续时间限制为3秒。
-    # -i anullsrc=r=44100:cl=stereo：使用anullsrc滤镜生成一个静音音频流，采样率为44100 Hz，立体声。
-    # -c:v copy：直接复制视频流，不重新编码。
-    # -c:a aac：将音频流编码为AAC格式。
-    subprocess.run(ffmpeg_command, shell=True, check=True)  
     
-
-# img + mp3 ---> video
-def img_mp3_to_mp4(mp3_file, img_file, size, mp4_file):
-    # print("img_mp3_to_mp4")
-    ffmpeg_cmd = (
-        f"ffmpeg -loglevel error  -i {mp3_file} "
-        f" -loop 1 -i {img_file}"
-        f" -vcodec libx264 -pix_fmt yuv420p -shortest -y "
-        f" -vf 'scale={size[0]}:{size[1]}' {mp4_file}"
-    )
-    # print(ffmpeg_cmd)
-    subprocess.call(ffmpeg_cmd, shell=True)
-
-    
-# img ---> video
-def image_to_video(image_path, size, output_file):
-    # FFmpeg 命令
-    # -vf 'scale=2204:1240'
-    ffmpeg_command = (
-        f"ffmpeg -loglevel error -loop 1 -t 3 -i {image_path}"
-        f" -f lavfi -i anullsrc=cl=mono:r=24000 -shortest -y "
-        f" -vf 'scale={size[0]}:{size[1]}' {output_file}"
-        
-    )
-    
-    # 运行 FFmpeg 命令
-    subprocess.run(ffmpeg_command, shell=True, check=True)
-        
-
 # 将多个短视频合成为一个视频
 def concatenate_mp4_files(input_txt, output_file):
     
@@ -364,5 +365,58 @@ def concatenate_mp4_files(input_txt, output_file):
     
     # 运行FFmpeg命令合并视频并覆盖已有的output.mp4
     subprocess.call(ffmpeg_command, shell=True)
-    print("Videos have been merged into output.mp4")
+    print("Videos have been merged into output.mp4")    
+            
+        
+# 删除单个文件
+def delete_file(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        print(f"{file_path} has been deleted")
+    else:
+        print(f"{file_path} does not exist")
 
+
+# 删除文件夹及其内容
+def delete_folder(folder_path):
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+        print(f"{folder_path} and its contents have been deleted")
+    else:
+        print(f"{folder_path} does not exist")
+   
+
+# 创建空白静音视频
+def create_silent_video(duration, size, output_file):
+    # 创建一个白色的静音视频
+    ffmpeg_command = (
+        f"ffmpeg -loglevel error -f lavfi -i color=c=white:s={size[0]}x{size[1]}:d={duration} "
+        f"-f lavfi -t {duration} -i anullsrc=r=24000:cl=mono "
+        f" -c:v libx264 -c:a aac -y {output_file}"
+    )
+    # -i input.mp4：指定输入视频文件。
+    # -f lavfi -t 3 -i anullsrc=r=44100:cl=stereo：
+    # -f lavfi：指定输入格式为Libavfilter。
+    # -t 3：将输入流的持续时间限制为3秒。
+    # -i anullsrc=r=44100:cl=stereo：使用anullsrc滤镜生成一个静音音频流，采样率为44100 Hz，立体声。
+    # -c:v copy：直接复制视频流，不重新编码。
+    # -c:a aac：将音频流编码为AAC格式。
+    subprocess.run(ffmpeg_command, shell=True, check=True)  
+       
+
+def create_silent_mp3(duration, output_file):
+    # Construct the ffmpeg command
+    command = [
+        'ffmpeg', 
+        '-loglevel', 'error',
+        '-f', 'lavfi', 
+        '-i', 'anullsrc=r=44100:cl=stereo', 
+        '-t', str(duration), 
+        '-y', # 覆盖原有文件
+        output_file
+    ]
+
+    # Run the command
+    subprocess.run(command, check=True)
+    print("Done! Create silent mp3")
+    return output_file
